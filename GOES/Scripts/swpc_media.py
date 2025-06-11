2493a9596deb781a082768efd297c487e627b403
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-06-11 15:10:03 -0400

    include pull and formatting of solar regions and magnetogram instead of synoptic map

--- GOES/Scripts/swpc_media.py
+++ GOES/Scripts/swpc_media.py
@@ -5,11 +5,19 @@
 :Author: W. Aaron (william.aaron@cfa.harvard.edu)
 :Last Updated: Apr 09, 2025
 
+:INFO:
+    - https://sdo.gsfc.nasa.gov/data/rules.php
+    - https://sdo.gsfc.nasa.gov/assets/docs/HMI_M.ColorTable.pdf
+
 """
 import os
-import sys
 import argparse
+import json
+import math
 from urllib.parse import urlparse
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from astropy.table import Table
 #
 #--- Define Directory Pathing
 #
@@ -18,10 +26,13 @@ GOES_MEDIA_DIR = '/data/mta4/www/RADIATION_new/GOES/Media'
 # --- Links to media sources
 #
 CCOR_1_7DAYS = 'https://services.swpc.noaa.gov/products/ccor1/mp4s/ccor1_last_7_days.mp4'
-SOLAR_MAP = "https://services.swpc.noaa.gov/images/synoptic-map.jpg"
-URL_FILES = [CCOR_1_7DAYS,SOLAR_MAP]
+MAGNETOGRAM_MAP = 'https://sdo.gsfc.nasa.gov/assets/img/latest/latest_4096_HMIBC.jpg'
+SOLAR_REGIONS = 'https://services.swpc.noaa.gov/json/solar_regions.json'
 
-def pull_swpc_media():
+URL_FILES = [CCOR_1_7DAYS,MAGNETOGRAM_MAP,SOLAR_REGIONS]
+TODAY = datetime.now().strftime("%Y-%m-%d")
+
+def swpc_media():
     """
     Periodically pull SWPC media for GOES web pages.
     """
@@ -29,6 +40,12 @@ def pull_swpc_media():
         file_name = os.path.basename(urlparse(url).path)
         cmd = f"wget -O {GOES_MEDIA_DIR}/{file_name} {url}"
         os.system(cmd)
+    
+
+    with open(os.path.basename(urlparse(SOLAR_REGIONS).path)) as f:
+        raw_json = json.load(f)
+        all_regions_table = Table(rows = raw_json)
+        todays_regions = all_regions_table[all_regions_table['observed_date'] == TODAY]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -44,7 +61,7 @@ if __name__ == "__main__":
             GOES_MEDIA_DIR = f"{OUT_DIR}/GOES/Media"
         os.makedirs(GOES_MEDIA_DIR, exist_ok=True)
 
-        pull_swpc_media()
+        swpc_media()
 
     elif args.mode == "flight":
-        pull_swpc_media()
+        swpc_media()
